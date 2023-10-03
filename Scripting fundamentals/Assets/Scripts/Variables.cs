b88bf335351b73fe2649a443a183f9f14afec532
Correa1310 <146136159+Correa1310@users.noreply.github.com>
2023-10-03 14:22:18 -0700

    Creating new variables

---
+++ Scripting fundamentals/Assets/Scripts/Variables.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    //Public Global/Member Variables
+    public int Score = 0; // declare a whole number
+    public float ItemAmount = 19.95f; //decimal number
+    public string PlayerName = "Frazbear"; // declare a text 
+    public bool IsPlayerActive = true; //declare a true/false statement 
+
+    //private global/member variables 
+    private int _colletctibleAmount = 0;    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
